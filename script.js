70fb6305d5542980ec623445bf9a1dce3148c54c
Yves Dorfsman <yves@zioup.com>
2023-10-27 19:34:51 -0600

    handleClick

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 console.log("👋 🌎");
+const root = document.getElementById("root");
+
+function handleClick(e) {
+  console.dir(e)
+}
+
+
+let button = document.createElement("button");
+root.appendChild(button);
+button.innerText = "X";
+button.classList.add("square");
+
+button.setAttribute("onclick", "handleClick(this)")
+
